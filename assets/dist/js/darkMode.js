c50b221f94ae8bb88609ad83abb270daa1dbf334
saluscas <saa.luscas@gmail.com>
2023-04-30 11:38:40 -0300

    Active link of nav

--- assets/dist/js/darkMode.js
+++ assets/dist/js/darkMode.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $("body").toggleClass("");
         $("nav").toggleClass("navbar-dark bg-dark");
         
-        $("footer").toggleClass("bg-dark");
+        $("footer").toggleClass("navbar-dark text-light bg-dark");
         $("#content").toggleClass("navbar-dark bg-dark");
         $("a").toggleClass("navbar-dark bg-dark");
         $("#sidebarCollapse").toggleClass("navbar-dark bg-dark");
@@ -11,12 +11,16 @@ $(document).ready(function() {
         $("#selector").toggleClass("navbar-dark bg-dark");
         $("#content > section > div > div > div.col-lg-4 > div:nth-child(1) > div").toggleClass("navbar-dark bg-dark");
         $("#sidebar > ul").toggleClass("navbar-secondary bg-secondary");
-
-
+        
+        
+        $("#content > hr").toggleClass("text-secondary");
+        $(".navbar-nav .show > .nav-link, .navbar-nav .nav-link.active").toggleClass("text-active");
+        
+       
+        
+        
         
         $("ul").toggleClass("navbar-dark bg-dark");
 
-        $("h1").toggleClass("text-white");
-        $("h2").toggleClass("text-white");
     });
 });
