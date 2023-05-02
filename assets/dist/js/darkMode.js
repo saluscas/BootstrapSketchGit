d2646df80c32f08b8a8c3fab685e6b726db6c1d2
saluscas <saa.luscas@gmail.com>
2023-05-02 02:10:48 -0300

    Collapses of nav and DarkMode.

--- assets/dist/js/darkMode.js
+++ assets/dist/js/darkMode.js
@@ -7,8 +7,24 @@ $(document).ready(function() {
         $("footer").toggleClass("navbar-dark text-light bg-dark");
         $("#content").toggleClass("navbar-dark bg-dark");
         $("a").toggleClass("navbar-dark bg-dark");
+
         $("#sidebarCollapse").toggleClass("navbar-dark bg-dark");
         $("#barCollapse").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseTwo").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseTwo").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseThree").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseThree").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseFour").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseFour").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseFive").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseFive").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseSix").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseSix").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseSeven").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseSeven").toggleClass("navbar-dark bg-dark");
+        $("#sidebarCollapseEight").toggleClass("navbar-dark bg-dark");
+        $("#barCollapseEight").toggleClass("navbar-dark bg-dark");
+
         $("#selector").toggleClass("navbar-dark bg-dark");
         $("#content > section > div > div > div.col-lg-4 > div:nth-child(1) > div").toggleClass("navbar-dark bg-dark");
         $("#sidebar > ul").toggleClass("navbar-secondary bg-secondary");
@@ -26,7 +42,8 @@ $(document).ready(function() {
         $("#carouselExampleCaptions > div.carousel-indicators > button:nth-child(7)").toggleClass("bg-white");
         $("#carouselExampleCaptions > div.carousel-indicators > button:nth-child(8)").toggleClass("bg-white");
        
-        
+
+
         
         
         $("ul").toggleClass("navbar-dark bg-dark");
