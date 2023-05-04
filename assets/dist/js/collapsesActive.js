fe54f008420471d592b015ac7fe94df5414304a1
saluscas <saa.luscas@gmail.com>
2023-05-03 21:08:42 -0300

    Fixing incongruity in js toggles: $('#sidebarCollapse').toggleClass('active');

--- assets/dist/js/collapsesActive.js
+++ assets/dist/js/collapsesActive.js
@@ -1,99 +1,176 @@
-    $(document).ready(function () {
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapse').on('click', function () {
-            $(this).toggleClass('active');
-        });
-    });
+});
+$(document).ready(function () {
+    $('#barCollapse').on('click', function () {
+        $(this).toggleClass('active');
+});
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseTwo').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseTwo').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseTwo').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseTwo').on('click', function () {
+        $(this).toggleClass('active');
     });
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseThree').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseThree').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseThree').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseThree').on('click', function () {
+        $(this).toggleClass('active');
+        $('#navbarSupportedContentTwo').collapse({
+            toggle: false
+        })
+        $('#navbarSupportedContentThree').collapse({
+            toggle: false
+        })
+        $('#navbarSupportedContentFour').collapse({
+            toggle: false
+        })
+        $('#navbarSupportedContentFive').collapse({
+            toggle: false
+        })
+        $('#navbarSupportedContentSix').collapse({
+            toggle: false
+        })
+        $('#navbarSupportedContentSeven').collapse({
+            toggle: false
+        })
+        $('#navbarSupportedContentEight').collapse({
+            toggle: false
+        })
     });
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseFour').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseFour').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseFour').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseFour').on('click', function () {
+        $(this).toggleClass('active');
     });
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseFive').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseFive').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseFive').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseFive').on('click', function () {
+        $(this).toggleClass('active');
     });
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseSix').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseSix').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseSix').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseSix').on('click', function () {
+        $(this).toggleClass('active');
     });
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseSeven').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseSeven').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseEight').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseSeven').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseSeven').on('click', function () {
+        $(this).toggleClass('active');
     });
+});
 
-    $(document).ready(function () {
-        $('#sidebarCollapseEight').on('click', function () {
-            $('#sidebar').toggleClass('active');
-            $(this).toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapseEight').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('active');
+        $('#sidebarCollapseTwo').toggleClass('active');
+        $('#sidebarCollapseThree').toggleClass('active');
+        $('#sidebarCollapseFour').toggleClass('active');
+        $('#sidebarCollapseFive').toggleClass('active');
+        $('#sidebarCollapseSix').toggleClass('active');
+        $('#sidebarCollapseSeven').toggleClass('active');
+        $(this).toggleClass('active');
     });
-    $(document).ready(function () {
-        $('#barCollapseEight').on('click', function () {
-            $(this).toggleClass('active');
-        });
+});
+$(document).ready(function () {
+    $('#barCollapseEight').on('click', function () {
+        $(this).toggleClass('active');
     });
+});
 
-    $('.carousel').carousel({
-        interval: 1000000
-    })    
+$('.carousel').carousel({
+    interval: 1000000
+})    
